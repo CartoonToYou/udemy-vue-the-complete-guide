85d58e0b35d244006b06453354392a48d71ae9f7
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-05-21 23:17:23 +0700

    Finished: basic communication components

---
+++ section-5_VueJSRealWorld/communication-components/src/components/User.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <p>Name is {{ name }}</p>
+    <p>{{ age }}</p>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail
+          :myName="name"
+          @nameWasReset="name = $event"
+          :resetFn="resetName"
+          :userAge="age"
+        ></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit
+          :userAge="age"
+          @ageWasEdited="age = $event"
+        ></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data: function () {
+    return {
+      name: "Toon",
+      age: 27,
+    };
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit,
+  },
+  methods: {
+    changeName() {
+      this.name = "Ana";
+    },
+    resetName() {
+      this.name = "Toon";
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
