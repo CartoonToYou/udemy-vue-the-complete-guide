68013cb4e4f8eb9c9f934db505944e9b9af1154a
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-05-29 11:36:31 +0700

    Finished: Filter and Mixins

--- section-8_DirectivesFiltersMixins/src/FilterAndMixins.vue
+++ section-8_DirectivesFiltersMixins/src/FilterAndMixins.vue
@@ -5,36 +5,38 @@
       <!-- prettier-ignore -->
       <p>{{ text | toUppercase | to-lowercase }}</p>
       <hr />
+      <button @click="fruits.push('Berries')">Add New Item</button>
       <input v-model="filterText" />
       <ul>
         <li v-for="fruit in filteredFruits">{{ fruit }}</li>
       </ul>
+      <hr />
+      <app-list></app-list>
     </div>
   </div>
 </template>
 
 <script>
+import List from "./List.vue";
+import { fruitMixin } from "./fruitMixin";
+
 export default {
+  components: {
+    appList: List,
+  },
+
   filters: {
     toUppercase(value) {
       return value.toUpperCase();
     },
   },
 
+  mixins: [fruitMixin],
+
   data() {
     return {
-      text: "Hello there!",
-      fruits: ["Apple", "Banana", "Mango", "Melon"],
-      filterText: "",
+      text: "Hello There!",
     };
-  },
-
-  computed: {
-    filteredFruits() {
-      return this.fruits.filter((element) => {
-        return element.match(this.filterText);
-      });
-    },
   },
 };
 </script>
