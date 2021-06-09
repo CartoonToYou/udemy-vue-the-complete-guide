bb73b39f5ce596ed6c199db91ac6a02dc5e9d2c3
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-06-09 22:36:58 +0700

    Finished Vuex

---
+++ section-12_Vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button
+      class="btn btn-primary"
+      @click="asyncIncrement({ by: 50, duration: 500 })"
+    >
+      Increment
+    </button>
+    <button
+      class="btn btn-primary"
+      @click="asyncDecrement({ by: 50, duration: 500 })"
+    >
+      Decrement
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+/* mutations use commit('functionName') */
+/* actions use dispatch('functionName') */
+
+export default {
+  methods: {
+    ...mapActions(["asyncIncrement", "asyncDecrement"]),
+    /* default form of actions */
+    // increment(by) {
+    //   this.$store.dispatch("increment", by);
+    // },
+  },
+};
+</script>
--- section-12_Vuex/src/components/Result.vue
+++ section-12_Vuex/src/components/Result.vue
@@ -1,9 +1,24 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+  <p>Counter is: {{ counter }}</p>
 </template>
 
 <script>
-    export default {
-        props: ['counter']
-    }
+import { mapGetters } from "vuex";
+import * as types from "../store/types";
+
+/* mutations use commit('functionName') */
+/* actions use dispatch('functionName') */
+
+export default {
+  methods: {
+    // ...mapActions(["asyncIncrement", "asyncDecrement"]),
+    /* default form of actions */
+    // increment(by) {
+    //   this.$store.dispatch("increment", by);
+    // },
+    ...mapGetters({
+      counter: types.DOUBLE_COUNTER,
+    }),
+  },
+};
 </script>
