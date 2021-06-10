40ef305b1faa57343d7f6cf02bfd3eb060d0f3e2
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-06-11 00:20:37 +0700

    create Stocks and stock component from lectire

--- section-13_TheStockTraderApp/src/components/stocks/Stock.vue
+++ section-13_TheStockTraderApp/src/components/stocks/Stock.vue
@@ -1,25 +1,53 @@
 <template>
-  <div>
-    <p>The Stocks -Stock Component</p>
-    <p>{{ name }}: {{ price }}</p>
-    <input type="number" v-model="stocksQty" />
-    <button @click="confirmBuyStocks">Buy!</button>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }} <small>(Price: {{ stock.price }})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Quantity"
+            v-model.number="quantity"
+          />
+        </div>
+        <div class="pull-right">
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          >
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["name", "price"],
+  props: ["stock"],
 
   data() {
     return {
-      stocksQty: 0,
+      quantity: 0,
     };
   },
 
   methods: {
-    confirmBuyStocks() {
-      this.$emit("buyStocks", this.stocksQty);
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      console.log(order);
+      this.quantity = 0;
     },
   },
 };
--- section-13_TheStockTraderApp/src/components/stocks/Stocks.vue
+++ section-13_TheStockTraderApp/src/components/stocks/Stocks.vue
@@ -1,13 +1,6 @@
 <template>
-  <div class="row">
-    <h1>The Stocks Component</h1>
-    <div v-for="stock in stocks" :key="stock.id" class="col">
-      <app-stock
-        :name="stock.name"
-        :price="stock.price"
-        @buyStocks="getStocks"
-      />
-    </div>
+  <div>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
   </div>
 </template>
 
@@ -22,20 +15,26 @@ export default {
   data() {
     return {
       stocks: [
-        { id: 1, name: "BMW", price: 20 },
+        { id: 1, name: "BMW", price: 110 },
         {
           id: 2,
           name: "Google",
-          price: 50,
+          price: 200,
+        },
+        {
+          id: 3,
+          name: "Apple",
+          price: 250,
+        },
+        {
+          id: 4,
+          name: "Twitter",
+          price: 8,
         },
       ],
     };
   },
 
-  methods: {
-    getStocks(qty) {
-      console.log(qty);
-    },
-  },
+  methods: {},
 };
 </script>
